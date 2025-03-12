ae45af86d064d5f57fe2896ce5f996ec3c2851c2
imran-bin <engg.imran22@gmail.com>
2025-03-12 11:49:47 +0600

    addition practice

--- c_practice_question.c
+++ c_practice_question.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
-main(){
-    
+int main(){
+    int i,j,a,s,d,m;
+    scanf("%d %d",&i,&j);
+    a=i+j;
+    s=i-j;
+    d=i/j;
+    m=i*j;
+    printf ("%d+%d = ",i,j,a);
+
 }
